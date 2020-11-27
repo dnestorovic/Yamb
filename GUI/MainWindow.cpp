e1f630b0fff7bd18f9f42caf280e63fc8958fddc
sm998 <stefan_marjanovic_@hotmail.com>
2020-11-27 12:56:48 +0100

    added sound effects

--- GUI/MainWindow.cpp
+++ GUI/MainWindow.cpp
@@ -4,9 +4,12 @@
 Widget::Widget(QWidget *parent)
     : QWidget(parent)
     , ui(new Ui::Widget)
+    , m_click_sound(this)
 {
     ui->setupUi(this);
 
+
+    //setup for both tables
     setWidthForTable(ui->tableL,m_column_width);
     ui->tableL->setStyleSheet(("QTableWidget {border : 5px solid rgb(114, 159, 207);}"));
 
@@ -17,10 +20,22 @@ Widget::Widget(QWidget *parent)
     ui->tableL->setSpan(0,10,6,1);
     ui->tableR->setSpan(0,10,6,1);
 
+    //we hide text edit with help so it could be seen only when ask button is clicked
     ui->plainTextEdit->hide();
-
     connect(ui->btnAsk,&QPushButton::clicked,this,&Widget::hideText);
 
+    //setting-up click sound
+    m_click_sound.setSource(QUrl::fromLocalFile(":/sounds/sound-click"));
+    m_click_sound.setVolume(0.75f);
+
+    connect(ui->btnAsk, &QPushButton::clicked, &m_click_sound, &QSoundEffect::play);
+    connect(ui->btnSend, &QPushButton::clicked, &m_click_sound, &QSoundEffect::play);
+    connect(ui->btnThrow, &QPushButton::clicked, &m_click_sound, &QSoundEffect::play);
+    connect(ui->btnMute, &QPushButton::clicked, &m_click_sound, &QSoundEffect::play);
+
+    connect(ui->btnMute,&QPushButton::clicked, this, &Widget::decreaseVolume);
+
+    connect(ui->btnSend,&QPushButton::clicked, this, &Widget::sendMessage);
 }
 
 Widget::~Widget()
@@ -37,6 +52,43 @@ void Widget::hideText()
     }
     else{
         ui->plainTextEdit->show();
+    }
+}
+
+void Widget::decreaseVolume()
+{
+    switch(m_volume_intensity){
+
+        case full:  m_volume_intensity = mid;
+                    m_click_sound.setVolume(0.5f);
+                    ui->btnMute->setIcon(QIcon(":/img/img-speaker_m"));
+                    break;
+        case mid:   m_volume_intensity = low;
+                    m_click_sound.setVolume(0.25f);
+                    ui->btnMute->setIcon(QIcon(":/img/img-speaker_l"));
+                    break;
+        case low:   m_volume_intensity = mute;
+                    m_click_sound.setVolume(0.0f);
+                    ui->btnMute->setIcon(QIcon(":/img/img-speaker_mute"));
+                    break;
+        case mute:  m_volume_intensity = full;
+                    m_click_sound.setVolume(0.75f);
+                    ui->btnMute->setIcon(QIcon(":/img/img-speaker_f"));
+                    break;
+        default: ;
+
+    }
+}
+
+void Widget::sendMessage()
+{
+    int last_item_index = ui->lChat->count();
+    if(ui->leMessage->text().size() > 0){
+
+        ui->lChat->addItem(ui->leMessage->text());
+        ui->lChat->item(last_item_index)->setTextAlignment(Qt::AlignRight);
+        ui->leMessage->setText("");
+
     }
 }
 
--- GUI/MainWindow.h
+++ GUI/MainWindow.h
@@ -2,8 +2,11 @@
 #define WIDGET_H
 
 #include <QWidget>
+#include <QSoundEffect>
 
 class QTableWidget;
+
+enum volume_intensity{mute,low,mid,full};
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class Widget; }
@@ -17,6 +20,8 @@ public:
     Widget(QWidget *parent = nullptr);
     ~Widget();
     void hideText();
+    void decreaseVolume();
+    void sendMessage();
 
 private:
     void setWidthForTable(QTableWidget *table, int width);
@@ -24,5 +29,7 @@ private:
 private:
     Ui::Widget *ui;
     const int m_column_width = 30;
+    QSoundEffect m_click_sound;
+    volume_intensity m_volume_intensity = full;
 };
 #endif // WIDGET_H
